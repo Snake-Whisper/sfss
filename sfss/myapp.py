02821ea2e75a296d5a9e612b9c1e49f537beeecf
Snake-Whisper <34867916+Snake-Whisper@users.noreply.github.com>
2019-02-15 19:17:27 +0100

    Update myapp.py

--- sfss/myapp.py
+++ sfss/myapp.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hi():
-	return "Hello World, how are you! Is the program waiting for changes?!?"
+	return "Hello World, how are you! Is the program waiting for changes?"
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
