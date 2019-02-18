7071aebd09aa5802f90b8d40978e44d6b50ce89e
snake-whisper <snake-whisper@web-utils.ml>
2019-02-18 18:05:31 +0100

    updated schema.sql

--- sfss/schema.sql
+++ sfss/schema.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF NOT EXISTS users;
 CREATE TABLE users (
-	id int primary key autoincrement,
-	username char(50) not null,
-	password
+	id INT primary key AUTO_INCREMENT,
+	username VARCHAR(50) not null,
+	password VARCHAR(41) not null,
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
+	email VARCHAR(75),
+	enabled boolean default true) ENGINE=INNODB;
