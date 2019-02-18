ae8755e8b2a96478055a2ee6cc942c3c198405af
snake-whisper <snake-whisper@web-utils.ml>
2019-02-18 14:29:34 +0100

    .

--- sfss/myapp.py
+++ sfss/myapp.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hi():
 	return "Hello World, how are you! Is the program waiting for changes?"
-if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+	if request.method == 'POST':
+		return request.form["username"]
+	return render_template("login.html")#, name=user)
+	
+
+#if __name__ == "__main__":
+#	app.run(host='0.0.0.0')
