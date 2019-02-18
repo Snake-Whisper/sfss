76f1f76d4220cde559b4f43705d8312ad8a8f5c9
snake-whisper <snake-whisper@web-utils.ml>
2019-02-18 20:01:21 +0100

    add registerUser and chkLogin funct

--- sfss/myapp.py
+++ sfss/myapp.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, request, session, escape, url_for, redirect, g
+import random
+import pymysql
 app = Flask(__name__)
-app.secret_key = '\x81H\xb8\xa3S\xf8\x8b\xbd"o\xca\xd7\x08\xa4op\x07\xb5\xde\x87\xb8\xcc\xe8\x86\\\xffS\xea8\x86"\x97'
+app.config.update(
+	SECRET_KEY = '\x81H\xb8\xa3S\xf8\x8b\xbd"o\xca\xd7\x08\xa4op\x07\xb5\xde\x87\xb8\xcc\xe8\x86\\\xffS\xea8\x86"\x97'
+)
 
-def getDB():
+def getDBCursor():
 	if not hasattr(g, 'db'):
 		g.db = pymysql.connect(user='sfss', password='QsbPu7N0kJ4ijyEf', db='sfss', cursorclass=pymysql.cursors.DictCursor)
-	return g.db
+	return g.db.cursor()
 
 @app.teardown_appcontext
-def closeDB():
+def closeDB(error):
+	print(error)
 	if hasattr(g, 'db'):
-		db.close()
-		
+		g.db.commit()
+		g.db.close()
+
+def rndFill():
+	cursor = getDBCursor()
+	for i in range (100):
+		cursor.execute("INSERT INTO users (username, password) VALUES ({0}, {0})".format(i))
+	cursor.close()
+
+def registerUser(username, password, firstName="null", lastName="null", email="null", enabled=1):
+	cursor = getDBCursor()
+	cursor.execute("INSERT INTO users (username, password, firstName, lastName, email, enabled) VALUES (\'{0}\', PASSWORD(\'{1}\'), \'{2}\', \'{3}\', \'{4}\', \'{5}\')".format(username, password, firstName, lastName, email, enabled))
+	cursor.close()
+
+def chkLogin(username, password):
+	cursor = getDBCursor()
+	cursor.execute("SELECT id FROM users WHERE username=\'{0}\' AND password=PASSWORD(\'{1}\')".format(username, password))
+	print(cursor.fetchall())
+	cursor.close()
 
 @app.route("/")
 def hi():
+	chkLogin("User1", "Versuch")
 	return "Hello World, how are you! Is the program waiting for changes?"
 	
 
